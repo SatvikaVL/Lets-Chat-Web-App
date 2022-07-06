d7a85faa09413f55b0ebf61cbde181bca4f6fa8a
SatvikaVL <100283639+SatvikaVL@users.noreply.github.com>
2022-07-06 17:01:26 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,36 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAW9JVL2mQzszCxrK41j3sgvDkHKAM3qqM",
+    authDomain: "lets-chat-web-app-db.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-db-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-db",
+    storageBucket: "lets-chat-web-app-db.appspot.com",
+    messagingSenderId: "186220052518",
+    appId: "1:186220052518:web:e1b365e1de945de30da6e7"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//ADD YOUR FIREBASE LINKS HERE
+
+
+
+
+
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    firebase.database().ref("/").child(user_name).update({
+        purpose : "adding user"
+    });
+    /*
+    localStorage.setItem("user_name", user_name);
+    
+    window.location = "chat_room.html";
+    */
+    localStorage.setItem("user_name", user_name);
+    window.location = "chat_room.html";
+}
+
+
+
